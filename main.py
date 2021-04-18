395f7c9957dafc6d349bc67e42d8bade93eed871
Shemar Jones <jones.shemar.sj@gmail.com>
2021-04-17 21:56:33 -0500

    Update main.py
    Now has options for DFS, BFS and DIJKSTRA. Not fully implemented though.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import sys
 # import graph
 # import traversals
 
+# Global Variables
+exit_options = ["EXIT", "0", ""]
+
 # ---------- Menu Options -----------------------------
 
 
@@ -28,12 +31,68 @@ def menu():
     print("######################################")
 
 
+def graph_menu():
+
+    # Present Menu
+    print("######################################")
+    print("# 0 - EXIT                           #")
+    print("# 1 - BFS | Breadth First            #")
+    print("# 2 - Dijkstra's                     #")
+    print("# 3 - DFS | Depth First              #")
+    print("######################################")
+
+
 # -----------------------------------------------------
 # Main for Graphs
 
 
 def graph_main():
-    print("Graph Status: Not ready")
+
+    # Valid Options
+    bfs_options = ["1", "BFS", "BREADTH", "BREATH"]
+    dfs_options = ["2", "DFS", "DEPTH"]
+    dijk_options = ["3", "DIJK", "DJ"]
+
+    # Infinite Loop
+    while (True):
+        # Present Menu & get user option
+        graph_menu()
+        option = input("# Option - ").rstrip()
+        option = option.upper()
+
+        # Check for valid options
+        if (option in bfs_options):
+            bfs_main()
+        elif (option in dfs_options):
+            dfs_main()
+        elif (option in dijk_options):
+            dijkstra_main()
+        elif (option in exit_options):
+            break
+        else:
+            print("\n--------------------------------------")
+            print(f"# {option} | Not Valid - Try Again")
+            print("--------------------------------------\n")
+
+
+# BFS Main
+# -----------------------------------------------------
+def bfs_main():
+    pass
+
+# DFS Main
+# -----------------------------------------------------
+
+
+def dfs_main():
+    pass
+
+# DIJKSTRA Main
+# -----------------------------------------------------
+
+
+def dijkstra_main():
+    pass
 
 # -----------------------------------------------------
 # Main for Traversals
@@ -43,12 +102,41 @@ def traversal_main():
     print("Traversal Status: Not ready")
 
 # -----------------------------------------------------
+# Create Graph
+
+
+def create_graph():
+    graph = dict()
+
+    print()
+    print("#--------------------------------------")
+    print("# Enter Format --> NODE : Neighbours")
+    print("#--------------------------------------")
+
+    user_input = "GO"
+    while (True):
+        user_input = input("# ").rstrip()
+        user_input = user_input.upper()
+
+        if len(user_input) == 0:
+            break
+        # -----------------------
+
+        # Split up input
+        broken = user_input.split(":")
+        node, neighbor = broken[0].strip(), broken[1]
+
+        # Assign values to graph
+        graph[node] = neighbor
+
+    print("#--------------------------------------")
+
+    return graph
 
 
 # Driver -----------------------------------------------------
 if __name__ == "__main__":
     # Menu Options
-    exit_options = ["EXIT", "0", ""]
     graph_options = ["1", "GRAPH", "GRAPHS"]
     traversal_options = ["2", "TRAVERSAL", "TRAVERSALS"]
 
